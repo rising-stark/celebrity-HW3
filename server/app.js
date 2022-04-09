024534fb1acec217c360c06cb15f5fe359c69f86
Harshit Kumar <harshit@Harshits-MacBook-Pro.local>
2022-04-10 02:11:59 +0530

    mongodb atlas instance connected

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const express = require("express");
+const mongooseConnect = require('./helpers/dbConnect');
 const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
@@ -9,4 +10,8 @@ app.use(cors);
 app.use(express.json());
 
 dotenv.config();
+
+mongooseConnect.dbconnect()
+                .on('error', (err) => console.log("connection to db failed"))
+
 server.listen(process.env.PORT, () => console.log(`Server started on ${process.env.PORT}`));
