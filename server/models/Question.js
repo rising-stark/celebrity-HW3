6905dcd91eec93d84cf65cc881ac2e4c0c9a762b
Harshit Kumar <harshit@Harshits-MacBook-Pro.local>
2022-04-10 02:16:57 +0530

    added user and question model to store/retrieve data from database

---
+++ server/models/Question.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const questionSchema = new mongoose.Schema({
+  imgUrl: {
+    type: String,
+    required: true,
+  },
+  option1: {
+    type: String,
+    required: true,
+  },
+  option2: {
+    type: String,
+    required: true,
+  },
+  option3: {
+    type: String,
+    required: true,
+  },
+  option4: {
+    type: String,
+    required: true,
+  },
+  correct_option: {
+    type: String,
+    required: true,
+  }
+});
+
+module.exports = mongoose.model("Question", userSchema);
---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  bestScore: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+}, { timestamps: true });
+
+// Generate Tokens to Verify User
+userSchema.methods.generateToken = async function(){
+  try {
+    let generatedToken = jwt.sign({_id : this._id}, process.env.SECRET_KEY);
+    this.tokens = this.tokens.concat({token : generatedToken});
+    await this.save();
+    return generatedToken;
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = mongoose.model("User", userSchema);
