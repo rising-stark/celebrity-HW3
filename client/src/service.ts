27167fdceffd5359e6f4e306a5b89d43e87ccd52
Harshit Kumar <harshit@Harshits-MacBook-Pro.local>
2022-04-13 08:06:12 +0530

    cleaned up service.ts and added fetchLeaderboard function

--- client/src/service.ts
+++ client/src/service.ts
@@ -1,71 +1,22 @@
-import axios from 'axios';
-import { CelebDatum } from './constants';
+import axios from "axios";
+import { CelebDatum, LeaderBoard } from "./constants";
 
-function pickRandomName(): Promise<string> {
-  return new Promise((resolve) => {
-    axios.get('data.json').then((res) => {
-      const randomIndex = Math.floor(Math.random() * res.data.length);
-      resolve(res.data[randomIndex].name);
-    });
-  });
-}
-
-function pickRandomNameOtherThan(name: string): Promise<string[]> {
-  return new Promise((resolve) => {
-    axios.get('data.json').then((res) => {
-      const names = [];
-      while (names.length <= 4) {
-        const randomIndex = Math.floor(Math.random() * res.data.length);
-        if (res.data[randomIndex].name !== name) {
-          names.push(res.data[randomIndex].name);
-        }
-      }
-      resolve(names);
-    });
-  });
-}
-
-function pickRandomNameWith(name: string): Promise<string[]> {
-  return new Promise((resolve) => {
-    axios.get('data.json').then((res) => {
-      const names: string[] = [];
-      while (names.length <= 2) {
-        const randomIndex = Math.floor(Math.random() * res.data.length);
-        const randomName = res.data[randomIndex].name;
-        if (randomName !== name && !names.includes(randomName)) {
-          names.push(res.data[randomIndex].name);
-        }
-      }
-      names.push(name);
-
-      const shuffled = names
-        .map((nameTem) => ({ nameTem, sort: Math.random() }))
-        .sort((a, b) => a.sort - b.sort)
-        .map(({ nameTem }) => nameTem);
-
-      resolve(shuffled);
-    });
-  });
-}
-
-function getImageByName(name: string): Promise<string> {
-  return new Promise<string>((resolve) => {
-    axios.get('data.json').then((res) => {
-      res.data.map((obj: CelebDatum) => {
-        if (obj.name === name) {
-          resolve(obj.image);
-        }
-      });
-    });
-  });
-}
+const host = "http://localhost:5000";
 
 function fetchAllCeleb(): Promise<CelebDatum[]> {
   return new Promise<CelebDatum[]>((resolve) => {
-    axios.get("http://localhost:5000/game").then((res) => {
+    axios.get(`${host}/game`).then((res) => {
       resolve(res.data.questions);
     });
   });
 }
 
-export { pickRandomName, pickRandomNameOtherThan, getImageByName, fetchAllCeleb, pickRandomNameWith };
+function fetchLeaderboard(): Promise<LeaderBoard[]> {
+  return new Promise<LeaderBoard[]>((resolve) => {
+    axios.get(`${host}/leaderboard`).then((res) => {
+      resolve(res.data.leaderboard);
+    });
+  });
+}
+
+export { fetchAllCeleb, fetchLeaderboard };
