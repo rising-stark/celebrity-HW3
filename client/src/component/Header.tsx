50c282c7b13e91c1ea9b26908a6ac9fc04edb080
Harshit Kumar <harshit@Harshits-MacBook-Pro.local>
2022-04-13 07:23:53 +0530

    [minor] eslint mods

--- client/src/component/Header.tsx
+++ client/src/component/Header.tsx
@@ -1,38 +1,38 @@
-import { PageHeader, Button } from 'antd';
-import { currentPlayerLS } from '../constants';
-import { Link, useNavigate } from 'react-router-dom';
-import { ExclamationCircleOutlined, StopOutlined } from '@ant-design/icons';
-import Modal from 'antd/lib/modal';
+import { PageHeader, Button } from "antd";
+import { currentPlayerLS } from "../constants";
+import { Link, useNavigate } from "react-router-dom";
+import { ExclamationCircleOutlined, StopOutlined } from "@ant-design/icons";
+import Modal from "antd/lib/modal";
 
 const Header = () => {
   const navigate = useNavigate();
   const onDelete = () => {
     Modal.confirm({
       closable: true,
-      title: 'Delete Account & Data associated with it?',
+      title: "Delete Account & Data associated with it?",
       icon: <StopOutlined />,
-      okText: 'Yes',
-      cancelText: 'No',
+      okText: "Yes",
+      cancelText: "No",
       onOk: () => {
         const player = localStorage.getItem(currentPlayerLS);
         localStorage.removeItem(currentPlayerLS);
         if (player) {
           localStorage.removeItem(player);
         }
-        navigate('/');
+        navigate("/");
       },
     });
   };
   const onSignOut = () => {
     Modal.confirm({
       closable: true,
-      title: 'Sign Out?',
+      title: "Sign Out?",
       icon: <ExclamationCircleOutlined />,
-      okText: 'Yes',
-      cancelText: 'No',
+      okText: "Yes",
+      cancelText: "No",
       onOk: () => {
         localStorage.removeItem(currentPlayerLS);
-        navigate('/');
+        navigate("/");
       },
     });
   };
@@ -50,25 +50,28 @@ const Header = () => {
         title="Guess The Celeb"
         subTitle="How well do you know your celebrities?"
         extra={[
-          <Button>
-            <Link data-cy="link-leadboard" style={{ color: 'blue' }} to="/leaderBoard">
+          <Button key={1}>
+            <Link
+              data-cy="link-leadboard"
+              style={{ color: "blue" }}
+              to="/leaderBoard">
               LeaderBoard
             </Link>
           </Button>,
           <Button
+            key={2}
             data-cy="button-delete"
             danger
             onClick={() => {
               onDelete();
-            }}
-          >
+            }}>
             Delete Account
           </Button>,
           <Button
+            key={3}
             onClick={() => {
               onSignOut();
-            }}
-          >
+            }}>
             Sign Out
           </Button>,
         ]}
